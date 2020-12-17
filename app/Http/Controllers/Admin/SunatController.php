58411233d69c8732ea9e9bf40ffc80abaecaee79
hmena1979 <hmena1979@google.com>
2020-12-17 17:38:45 -0500

    Respuesta error

--- app/Http/Controllers/Admin/SunatController.php
+++ app/Http/Controllers/Admin/SunatController.php
@@ -577,7 +577,10 @@ class SunatController extends Controller
         if (!$result->isSuccess()) {
             // Error en la conexion con el servicio de SUNAT
             //var_dump($result->getError());
-            return 'Error de conexión';
+            $factura->cdr = var_dump($result->getError());
+            $fact = $factura->save();
+            return redirect('/admin/factura/'.$factura->id.'/edit')->with('message', 'Problemas de conexión, vuelva a realizar el envío')->with('typealert', 'danger');
+            // return 'Error de conexión';
         }
         $factura->status = 4;
         $cdr = $result->getCdrResponse();
