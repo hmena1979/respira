621478a5f4f8025604e0ce859c881a013d289ba1
hmena1979 <hmena1979@google.com>
2020-12-18 00:17:49 -0500

    Orden dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -23,16 +23,20 @@ class DashboardController extends Controller
     {
         if(Auth::user()->doctor_id == 1){
             $historias = Historia::with(['pac','sta','tip','dr'])
-            ->select('id','paciente_id','item','status','tipo','doctor_id')
-            ->where('fecha', session('fecha'))
-            ->orderBy('doctor_id')
+            ->join('pacientes','historias.paciente_id','pacientes.id')
+            ->select('historias.id','historias.paciente_id','historias.item','historias.status','historias.tipo','historias.doctor_id')
+            ->where('historias.fecha', session('fecha'))
+            ->orderBy('historias.doctor_id')
+            ->orderBy('pacientes.razsoc')
             ->get();
         }else{
             $historias = Historia::with(['pac','sta','tip','dr'])
-            ->select('id','paciente_id','item','status','tipo','doctor_id')
-            ->where('doctor_id',Auth::user()->doctor_id)
-            ->where('fecha', session('fecha'))
-            ->orderBy('doctor_id')
+            ->join('pacientes','historias.paciente_id','pacientes.id')
+            ->select('historias.id','historias.paciente_id','historias.item','historias.status','historias.tipo','historias.doctor_id')
+            ->where('historias.doctor_id',Auth::user()->doctor_id)
+            ->where('historias.fecha', session('fecha'))
+            ->orderBy('historias.doctor_id')
+            ->orderBy('pacientes.razsoc')
             ->get();
         }
         
@@ -47,16 +51,20 @@ class DashboardController extends Controller
         session(['fecha' => $request->input('fecha')]);
         if(Auth::user()->doctor_id == 1){
             $historias = Historia::with(['pac','sta','tip','dr'])
-            ->select('id','paciente_id','item','status','tipo','doctor_id')
-            ->where('fecha', session('fecha'))
-            ->orderBy('doctor_id')
+            ->join('pacientes','historias.paciente_id','pacientes.id')
+            ->select('historias.id','historias.paciente_id','historias.item','historias.status','historias.tipo','historias.doctor_id')
+            ->where('historias.fecha', session('fecha'))
+            ->orderBy('historias.doctor_id')
+            ->orderBy('pacientes.razsoc')
             ->get();
         }else{
             $historias = Historia::with(['pac','sta','tip','dr'])
-            ->select('id','paciente_id','item','status','tipo','doctor_id')
-            ->where('doctor_id',Auth::user()->doctor_id)
-            ->where('fecha', session('fecha'))
-            ->orderBy('doctor_id')
+            ->join('pacientes','historias.paciente_id','pacientes.id')
+            ->select('historias.id','historias.paciente_id','historias.item','historias.status','historias.tipo','historias.doctor_id')
+            ->where('historias.doctor_id',Auth::user()->doctor_id)
+            ->where('historias.fecha', session('fecha'))
+            ->orderBy('historias.doctor_id')
+            ->orderBy('pacientes.razsoc')
             ->get();
         }
         // $historias = Historia::with(['pac','sta','tip','dr'])->select('id','paciente_id','item','status','tipo','doctor_id')
