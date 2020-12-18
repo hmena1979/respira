d9ad18b3214fb5f14828226333c8027976f23cff
hmena1979 <hmena1979@google.com>
2020-12-17 22:54:02 -0500

    Error CDR 5

--- app/Http/Controllers/Admin/SunatController.php
+++ app/Http/Controllers/Admin/SunatController.php
@@ -577,7 +577,7 @@ class SunatController extends Controller
         if (!$result->isSuccess()) {
             // Error en la conexion con el servicio de SUNAT
             //var_dump($result->getError());
-            $factura->cdr = $result->getError();
+            $factura->cdr = print_r($result->getError(),true);
             // $factura->cdr = $result->getCdrResponse();
             $fact = $factura->save();
             return redirect('/admin/factura/'.$factura->id.'/edit')->with('message', 'Problemas de conexión')->with('typealert', 'danger');
